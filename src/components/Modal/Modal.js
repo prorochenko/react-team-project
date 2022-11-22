d07d110f1f9a64e18ab6bdc9511fff37084cce91
prorochenko <33101252+prorochenko@users.noreply.github.com>
2022-11-22 23:44:45 +0200

    added buttons and style on modal window

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,7 +1,7 @@
 import css from './Modal.module.scss';
 import { createPortal } from 'react-dom';
 import { useEffect, useState } from 'react';
-
+// import closeBtn from '../../assets/icons/symbol-def.svg#icon-Close';
 const modalRoot = document.querySelector('#modal-root');
 
 const Modal = () => {
@@ -33,10 +33,63 @@ const Modal = () => {
   return createPortal(
     <div className={css.Modal__backdrop} onClick={handleBackdropClick}>
       <div className={css.Modal__content}>
-        <h1>Your recommended daily calorie intake is</h1>
-        <button type="button" onClick={onClose}>
-          Close
+        <button className={css.btnClose} onClick={onClose}>
+          <svg className={css.buttonclose__icon} width="25" height="25">
+            <use
+              href="../../assets/icons/symbol-defs.svg#icon-ticket1"
+              width="25px"
+              height="25px"
+            ></use>
+          </svg>
         </button>
+        {/* <svg>
+          <path d="M24.653 2.795l-2.448-2.448-9.705 9.705-9.705-9.705-2.448 2.448 9.705 9.705-9.705 9.705 2.448 2.448 9.705-9.705 9.705 9.705 2.448-2.448-9.705-9.705 9.705-9.705z" />
+        </svg> */}
+        {/* <svg>
+          <use href="../../assets/icons/symbol-defs.svg#icon-Close"></use>
+        </svg> */}
+        <div className={css.container}>
+          <h1 className={css.title}>
+            Your recommended daily calorie intake is
+          </h1>
+          <p className={css.number}>
+            2800 <span className={css.smallText}>ккал</span>
+          </p>
+          <div className={css.box}>
+            <h2 className={css.subtitle}>Foods you should not eat</h2>
+            <ol className={css.list}>
+              <li className={css.item}>Flour products</li>
+              <li className={css.item}></li>
+              <li className={css.item}></li>
+              <li className={css.item}></li>
+              <li className={css.item}></li>
+              <li className={css.item}></li>
+              <li className={css.item}></li>
+              <li className={css.item}></li>
+              <li className={css.item}></li>
+              <li className={css.item}></li> <li className={css.item}></li>
+              <li className={css.item}></li>
+              <li className={css.item}></li> <li className={css.item}></li>
+              <li className={css.item}></li>
+              <li className={css.item}></li> <li className={css.item}></li>
+              <li className={css.item}></li>
+              <li className={css.item}></li> <li className={css.item}></li>
+              <li className={css.item}></li>
+              <li className={css.item}></li> <li className={css.item}></li>
+              <li className={css.item}></li>
+              <li className={css.item}></li> <li className={css.item}></li>
+              <li className={css.item}></li>
+              <li className={css.item}></li> <li className={css.item}></li>
+              <li className={css.item}></li>
+              <li className={css.item}></li> <li className={css.item}></li>
+              <li className={css.item}></li>
+              <li className={css.item}></li>
+            </ol>
+          </div>
+          <button className={css.btn} type="button" onClick={onClose}>
+            Start losing weight
+          </button>
+        </div>
       </div>
     </div>,
     modalRoot
