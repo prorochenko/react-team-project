0a076a394b26a2f6e38df72f13f1e09661fe6e90
prorochenko <33101252+prorochenko@users.noreply.github.com>
2022-11-22 21:28:19 +0200

    added placeholders

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,46 @@
+import css from './Modal.module.scss';
+import { createPortal } from 'react-dom';
+import { useEffect, useState } from 'react';
+
+const modalRoot = document.querySelector('#modal-root');
+
+const Modal = () => {
+  let [modal, setModal] = useState(false);
+
+  const onClose = () => {
+    setModal(false);
+  };
+
+  const handleBackdropClick = e => {
+    if (e.currentTarget === e.target) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('keydown', escKeyDown);
+    return () => {
+      document.removeEventListener('keydown', escKeyDown);
+    };
+  });
+
+  const escKeyDown = e => {
+    if (e.code === 'Escape') {
+      onClose();
+    }
+  };
+
+  return createPortal(
+    <div className={css.Modal__backdrop}>
+      <div className={css.Modal__content}>
+        <h1>hey</h1>
+        <button type="button" onClick={onClose}>
+          Close
+        </button>
+      </div>
+    </div>,
+    modalRoot
+  );
+};
+
+export default Modal;
