38ee81b556839a77460b7144f04fb53987d2408f
prorochenko <33101252+prorochenko@users.noreply.github.com>
2022-11-22 13:24:23 +0200

    added basic files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
+import { Route, Routes, Navigate } from 'react-router-dom';
+// import { useDispatch } from 'react-redux';
+import { lazy } from 'react';
+
+import { Layout } from './Layout';
+const Login = lazy(() => import('../pages/Login'));
+const Register = lazy(() => import('../pages/Register'));
+const Dairy = lazy(() => import('../pages/Dairy'));
+const Calculator = lazy(() => import('../pages/Calculator'));
+
 export const App = () => {
+  //Щоб не виходило при перезавантаженні.
+  // const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   dispatch(fetchCurrentUser());
+  // }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Navigate to="/calculator" />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="calculator" element={<Calculator />} />
+          <Route path="dairy" element={<Dairy />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </>
   );
 };
