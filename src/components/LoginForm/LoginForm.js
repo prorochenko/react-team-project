c0af15befc71df90f3c222aacf90ecc3f04a9e3d
Buzanov Kostya <buzanov.kostya@gmail.com>
2022-11-23 19:18:28 +0200

    Update LoginForm.js

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -29,7 +29,7 @@ export const LoginForm = () => {
                 className={css.input}
                 type="email"
                 name="email"
-                placeholder="Name *"
+                placeholder="Email *"
               />
             </label>
             <label>
