d80acd8841c450dff684541048b7c763c4ab8bdc
prorochenko <33101252+prorochenko@users.noreply.github.com>
2022-11-24 00:11:51 +0200

    added loading component to index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
+import LoadingComponent from './components/Loader/Loader.jsx';
 
 import { App } from 'components/App';
 import './index.css';
@@ -11,7 +12,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+      <PersistGate loading={<LoadingComponent />} persistor={persistor}>
         <BrowserRouter basename="/slim-mom/">
           <App />
         </BrowserRouter>
