b09e4fb0973e98c8bb279b0834e6f17ba368d572
prorochenko <33101252+prorochenko@users.noreply.github.com>
2022-11-23 13:51:53 +0200

    added style and buttons for diary and calculator

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -1,16 +1,27 @@
 import scss from './UserMenu.module.scss';
+import { NavLink } from 'react-router-dom';
 
 const UserMenu = () => {
   return (
-    <div>
-      <p>Welcome, usrname</p>
-      <button
-        className={scss.btn}
-        type="button"
-        // onClick={() => dispatch(logOut())}
-      >
-        Exit
-      </button>
+    <div className={scss.box}>
+      <div className={scss.nav}>
+        <NavLink className={scss.text} to="/diary">
+          Diary
+        </NavLink>
+        <NavLink className={scss.text} to="/calculator">
+          Calculator
+        </NavLink>
+      </div>
+      <div className={scss.rightBar}>
+        <p>Welcome, Repeta</p>
+        <button
+          className={scss.btn}
+          type="button"
+          // onClick={() => dispatch(logOut())}
+        >
+          Exit
+        </button>
+      </div>
     </div>
   );
 };
