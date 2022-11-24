b16f5ba3a80008a2a5ab0e9ca2d44ed68cf85b06
prorochenko <33101252+prorochenko@users.noreply.github.com>
2022-11-24 21:02:57 +0200

    updated modal styles mobile

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,6 +1,7 @@
 import { Navigation } from '../Navigation/Navigation';
 import UserMenu from '../UserMenu/UserMenu';
 import AuthNav from '../AuthNav/AuthNav';
+// import { useEffect } from 'react';
 
 import css from './AppBar.module.scss';
 import { useSelector } from 'react-redux';
