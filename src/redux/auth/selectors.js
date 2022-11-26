9bb683684e632a8cd8994ac5f415980a30aeae4d
Buzanov Kostya <buzanov.kostya@gmail.com>
2022-11-26 23:17:30 +0200

    fix route register to login

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -3,6 +3,7 @@ export const selectIsLoggedIn = state => state.auth.isLoggedIn;
 export const selectUser = state => state.auth.user;
 
 export const selectIsRefreshing = state => state.auth.isRefreshing;
+export const selectIsRegister = state => state.auth.isRegister;
 export const selectRefreshToken = state => state.auth.refreshToken;
 export const selectUserId = state => state.auth.user.id;
 
