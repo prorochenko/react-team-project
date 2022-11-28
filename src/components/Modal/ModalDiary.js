c3b5c5318f8ed43cc38510bf65ee0c51348422e9
Mariana Mazurok <m.p.mazurok@gmail.com>
2022-11-28 15:37:26 +0200

    add modalDiary

--- src/components/Modal/ModalDiary.js
+++ src/components/Modal/ModalDiary.js
@@ -1,21 +1,116 @@
-import DiaryAddProductForm from 'components/DiaryComp/DiaryAddProductForm/DiaryAddProductForm';
 import { useCallback } from 'react';
-import { useDispatch } from 'react-redux';
-import { toggle } from 'redux/auth/authSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggle } from 'redux/products/products-slice';
 import ModalUniversal from './ModaUniversal';
+import { useState } from 'react';
 
-const ModalDiary = () => {
+import { addDay, fetchProduct } from 'redux/products/products-operations';
+import { selectProduct } from 'redux/products/products-selectors';
+
+import scss from './ModalDiary.module.scss';
+import moment from 'moment';
+
+export default function ModalDiary() {
+  const [date, setDate] = useState('');
   const dispatch = useDispatch();
   const onClose = useCallback(() => {
     dispatch(toggle(false));
   }, [dispatch]);
 
+  const products = useSelector(selectProduct);
+
+  const [product, setProduct] = useState('');
+  const [grams, setGrams] = useState('');
+
+  const idProduct = () => {
+    if (products) {
+      const index = products.findIndex(pro => pro.title.ru === product);
+      if (index >= 0) {
+        return products[index]._id;
+      }
+    }
+  };
+
+  // const idProduct = () => {
+  //   if (products) {
+  //     return products[0]._id;
+  //   }
+  // };
+
+  const productId = idProduct();
+  const handleChange = ({ target: { name, value } }) => {
+    switch (name) {
+      case 'product':
+        if (value.length > 2) {
+          dispatch(fetchProduct(value));
+        }
+
+        return setProduct(value);
+
+      case 'grams':
+        return setGrams(value);
+      default:
+        return;
+    }
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    const form = e.target;
+    const weight = +form.elements.grams.value;
+
+    dispatch(addDay({ date, productId, weight }));
+    setProduct('');
+    setGrams('');
+  };
+
+  const getCalendarDate = newDate => {
+    setDate(moment(newDate).format('yyyy-MM-DD'));
+  };
+
   return (
-    <ModalUniversal>
-      <div>Hello</div>
-      <DiaryAddProductForm />
+    <ModalUniversal onClick={getCalendarDate}>
+      <div className={scss.form}>
+        <form className={scss.formProduct} onSubmit={handleSubmit}>
+          <label className={scss.formLabel}>
+            <input
+              className={scss.formInput}
+              type="text"
+              name="product"
+              value={product}
+              onChange={handleChange}
+              placeholder="Enter product name"
+              list="list"
+              autoComplete="off"
+              required
+            />
+            {products?.length > 0 && (
+              <datalist id="list">
+                {products.map(product => (
+                  <option key={product._id} value={product.title.ru} />
+                ))}
+              </datalist>
+            )}
+          </label>
+          <label className={scss.formLabel}>
+            <input
+              className={scss.formInput + ' ' + scss.formInput__gram}
+              type="text"
+              pattern="[0-9]+"
+              name="grams"
+              value={grams}
+              onChange={handleChange}
+              placeholder="Grams"
+              required
+              autoComplete="off"
+            />
+          </label>
+          <button className={scss.btnOrange} type="submit" onClick={onClose}>
+            Add
+          </button>
+        </form>
+      </div>
     </ModalUniversal>
   );
-};
-
-export default ModalDiary;
+}
